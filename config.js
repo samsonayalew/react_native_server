b9c627478e8ce139c023d6a56982fec0ef872d0b
samsonayalew <samson.ayalew.et@gmail.com>
2023-05-21 08:50:42 +0300

    second

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 // const DATABASE = "mongodb+srv://dbuser:dbpassword@your_db_name.pbn7j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-export const DATABASE = "mongodb+srv://MongoDBAdmin:F2C5lhVTqlu66SKKeJcw@cluster0.8xpp7sv.mongodb.net/";
+const DATABASE = "mongodb+srv://MongoDBAdmin:F2C5lhVTqlu66SKKeJcw@cluster0.8xpp7sv.mongodb.net/";
 // export const DATABASE = "mongodb://0.0.0.0:27017/react";
 const JWT_SECRET = "some_secret_letters_numbers";
 const SENDGRID_KEY = "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";
